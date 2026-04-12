318bb9656803647275498cdd783375276fab4c21
RahulKumar1119 <suratirahulkumar29@gmail.com>
2026-04-12 11:02:16 +0530

    Build Error Fixed!

--- frontend/src/components/NotificationCenter.tsx
+++ frontend/src/components/NotificationCenter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useNotification } from '../context/NotificationContext';
 import { Notification } from '../types/index';
 import LoadingSpinner from './LoadingSpinner';
